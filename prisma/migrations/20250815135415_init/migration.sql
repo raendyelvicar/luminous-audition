2dda895ec3dfa2ec299180d47d7d0eedaf3bd8ad
raendy.elvicar@gmail.com <raendy.elvicar@gmail.com>
2025-08-16 03:05:35 +0700

    update prisma

--- prisma/migrations/20250815135415_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Candidate" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "name" TEXT NOT NULL,
-    "status" TEXT NOT NULL,
-    "message" TEXT NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
