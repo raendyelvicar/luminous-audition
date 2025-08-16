2adba5e753c88b7882e600b0d6b904b81b4bfcfc
raendy.elvicar@gmail.com <raendy.elvicar@gmail.com>
2025-08-16 15:05:58 +0700

    change favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,6 +25,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
+      <head>
+        <link rel='icon' type='image/png' sizes='16x16' href='/favicon.png' />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
